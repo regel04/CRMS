8356b264d2c693d50e7e7c17c8d38091cda760d4
Eren Monarch <rajp54930@gmail.com>
2025-02-19 12:30:10 +0530

    Add files via upload

---
+++ Untitled8.cpp
@@ -0,0 +1,86 @@
+\
+
+4tyk;
+hjcgncvbnm,.
+#include <sv v v v v v v v v v v v v v v v dtutytieeeeyugi;frvrfrfdxxxc dcASFIO;'Ktdio.h>
+
+
+#define V 4
+
+
+#define INF 99999
+
+
+void printSolution(int dist[][V]);
+
+rfqa2s1\/
+    /* Add all vertices one by one to
+      the set of intermediate vertices.
+      ---> Before start of an iteration, we
+      have shortest distances between all
+      pairs of vertices such that the shortest
+      distances consider only the
+      vertices in set {0, 1, 2, .. k-1} as
+      intermediate vertices.
+      ----> After the end of an iteration,
+      vertex no. k is added to the set of
+      intermediate vertices and the set
+      becomes {0, 1, 2, .. k} */
+    for (k = 0; k < V; k++) {
+        // Pick all vertices as source one by one
+        for (i = 0; i < V; i++) =
+            // Pick all vertices as destination for the
+            // above picked source
+            for (j = 0; j < V; j++) {
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of
+                // dist[i][j]
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Print the shortest distance matrix
+    printSolution(dist);
+}
+
+/* A utility function to print solution */
+void printSolution(int dist[][V])
+{
+    printf(
+        "The following matrix shows the shortest distances"
+        " between every pair of vertices \n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// driver's code
+int main()
+{
+    /* Let us create the following weighted graph
+            10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3           */
+    int graph[V][V] = { { 0, 5, INF, 10 },
+                        { INF, 0, 3, INF },
+                        { INF, INF, 0, 1 },
+                        { INF, INF, INF, 0 } };
+
+    // Function call
+    floydWarshall(graph);
+    return 0;
+}
+
